1e3c586290046db12c4d67e220a3b07ec3596aba
Stefan Hüsges <tronsha@gmx.de>
2018-05-11 13:27:23 +0200

    Add irc config

--- src/Service/IrcService.php
+++ src/Service/IrcService.php
@@ -1,15 +1,26 @@
 <?php
 
-declare (strict_types=1);
+declare (strict_types = 1);
 
 namespace App\Service;
 
 class IrcService
 {
+    private $server;
+    private $port;
+
+    public function __construct($server, $port)
+    {
+        $this->server = $server;
+        $this->port = $port;
+    }
+
     public function run($output)
     {
         $output->writeln([
-                __CLASS__,
-            ]);
+            __CLASS__,
+            $this->server,
+            $this->port,
+        ]);
     }
 }

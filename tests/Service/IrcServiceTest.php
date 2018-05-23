4cc6f2f5461d72df35341f27d113b69ed96bac9e
Stefan Hüsges <tronsha@gmx.de>
2018-05-23 08:43:04 +0200

    Add first dummy test

---
+++ tests/Service/IrcServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class IrcServiceTest extends \PHPUnit\Framework\TestCase
+{
+    public function invokeMethod(&$object, string $methodName, ...$parameters)
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $parameters);
+    }
+    
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}

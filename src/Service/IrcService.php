86458b9de1e22a79e15718997d3dc5de4cf4a8e3
Stefan Hüsges <tronsha@gmx.de>
2018-06-01 11:59:53 +0200

    Add docblocks

--- src/Service/IrcService.php
+++ src/Service/IrcService.php
@@ -13,6 +13,11 @@ class IrcService
     private $ircServerPassword;
     private $ircServerConnection;
 
+    /**
+     * @param string      $server
+     * @param int         $port
+     * @param string|null $password
+     */
     public function setIrcServer(string $server, int $port, string $password = null)
     {
         $this->ircServerName = $server;
@@ -20,6 +25,11 @@ class IrcService
         $this->ircServerPassword = $password;
     }
 
+    /**
+     * @return resource
+     *
+     * @throws CouldNotConnectException
+     */
     public function connectToIrcServer()
     {
         $errorString = '';
@@ -43,12 +53,18 @@ class IrcService
         return $this->ircServerConnection;
     }
 
+    /**
+     * @return string
+     */
     public function readFromIrcServer(): string
     {
         return trim((string) fgets($this->ircServerConnection, 4096));
     }
 
-    public function writeToIrcServer($text)
+    /**
+     * @param string $text
+     */
+    public function writeToIrcServer(string $text)
     {
         fwrite($this->ircServerConnection, $text . PHP_EOL);
     }

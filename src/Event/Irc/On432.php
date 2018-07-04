7b7fd910caa964b9da1cc2a0e406a2bf5c8da7e3
Stefan Hüsges <tronsha@gmx.de>
2018-07-04 10:27:09 +0200

    Add event on432

---
+++ src/Event/Irc/On432.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Event\Irc;
+
+class On432 extends AbstractIrcEvent
+{
+    public function on432()
+    {
+//        $randomNick = $this->getNickService()->getRandomNick();
+//        $this->getOutputService()->setOutput($randomNick);
+    }
+}

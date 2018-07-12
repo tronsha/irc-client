f1a80008fb00b70025670629fc607f36b6e81731
Stefan Hüsges <tronsha@gmx.de>
2018-07-12 08:31:49 +0200

    Add preform repository

---
+++ src/Repository/PreformRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Repository;
+
+use App\Entity\Preform;
+use Doctrine\ORM\EntityRepository;
+
+class PreformRepository extends EntityRepository
+{
+    /**
+     * @param string $network
+     * @return Preform[]|null
+     */
+    public function getPreform(string $network): ?array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.network = :network')
+            ->setParameter('network', $network)
+            ->getQuery();
+
+        return $qb->getResult();
+    }
+}

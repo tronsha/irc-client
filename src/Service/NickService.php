fc0749bafd36c119c435497d7cb910aeb7899022
Stefan Hüsges <tronsha@gmx.de>
2018-07-23 08:33:27 +0200

    Update nick service

--- src/Service/NickService.php
+++ src/Service/NickService.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Service;
 
@@ -18,6 +18,7 @@ class NickService
         if (null === $this->nick) {
             return $this->getRandomNick();
         }
+
         return $this->nick;
     }
 

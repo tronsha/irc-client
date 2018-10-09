9e145b94215088d905650f3781ed6add868e066c
Stefan Hüsges <Tronsha@gmx.de>
2018-10-09 20:02:50 +0200

    Update cron test

--- tests/Service/CronServiceTest.php
+++ tests/Service/CronServiceTest.php
@@ -140,6 +140,7 @@ class CronServiceTest extends \PHPUnit\Framework\TestCase
 //            [true, '0 12 * JAN-JUN *', 0, 12, 1, 1, 0],
             [true, '0 12 * JAN-JUN *', new \DateTime('2018-01-01 12:00:00')],
 //            [false, '0 12 * JUL-DEC *', 0, 12, 1, 1, 0],
+            [false, '0 12 * JUL-DEC *', new \DateTime('2018-01-01 12:00:00')],
         ];
     }
 }

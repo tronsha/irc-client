95cedccb0ac03d6f064140aaa3f7e2e50b16d511
Stefan Hüsges <tronsha@gmx.de>
2019-01-03 07:41:56 +0100

    Update cron test

--- tests/Service/CronServiceTest.php
+++ tests/Service/CronServiceTest.php
@@ -101,6 +101,8 @@ class CronServiceTest extends \PHPUnit\Framework\TestCase
             [false, '* * * 1 *', new \DateTime('2015-10-21 16:29:00')],
             [false, '29 * * 1 *', new \DateTime('2015-10-21 16:29:00')],
             [false, '30 * * 10 *', new \DateTime('2015-10-21 16:29:00')],
+            [false, '* 10 * 1 *', new \DateTime('2015-10-21 16:29:00')],
+            [false, '* 1 * 10 *', new \DateTime('2015-10-21 16:29:00')],
             [false, '* * * * 1', new \DateTime('2015-10-21 16:29:00')],
             [true, '* * 21 * 1', new \DateTime('2015-10-21 16:29:00')],
             [true, '* * 1 * 3', new \DateTime('2015-10-21 16:29:00')],
